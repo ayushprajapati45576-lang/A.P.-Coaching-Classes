82ca582ab7ecda9bb3feb4bacee5f8c074fe1ebb
Ayush Prajapati <ayushprajapati45576@gmail.com>
2026-09-19 19:50:36 +0530

    app

---
+++ frontend/public/sw.js
@@ -0,0 +1,12 @@
+// Basic Service Worker to enable PWA installability
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  // Empty fetch handler to satisfy Chrome PWA requirements
+});
